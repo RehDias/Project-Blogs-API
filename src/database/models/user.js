b5985cec7a14670f6fc44ebfd3e21104a6016bc0
Renata Dias <renata_dias96@live.com>
2022-07-13 17:29:49 -0300

    feat: Criação do modelo User
    Criação do arquivo models/user.js onde define o modelo User com os atributos id, displayName, email, password e imagem

---
+++ src/database/models/user.js
@@ -0,0 +1,31 @@
+const { DataTypes } = require('sequelize');
+
+const attr = {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+
+  displayName: {
+    type: DataTypes.STRING,
+  },
+
+  email: {
+    type: DataTypes.STRING,
+    unique: true,
+  },
+
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+
+  image: DataTypes.STRING,
+};
+
+module.exports = (sequelize) => {
+  const User = sequelize.define('User', attr, { tableName: 'Users' });
+  return User;
+};
